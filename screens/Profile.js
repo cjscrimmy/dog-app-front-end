15c3945bf869622ce53df592e474329c893ce560
digetl <dave@digetl.co.uk>
2020-07-16 15:23:44 +0100

    add text

--- screens/Profile.js
+++ screens/Profile.js
@@ -21,56 +21,31 @@ const Profile = ({ navigation }) => {
     <>
       <ScrollView>
         <SafeAreaView behavior="padding" style={styles.container}>
-          <View>
-            <Image
-              style={styles.logo}
-              source={require("../assets/lineup.png")}
-            ></Image>
-            <Text style={styles.title}>Next Best Friend</Text>
-          </View>
-        </SafeAreaView>
-      </ScrollView>
-
-      <TouchableOpacity style={(styles.buttonRow, styles.tanrow)}>
+      
         <View>
           <Text style={styles.buttonText}>Jayne MacDonald </Text>
         </View>
-      </TouchableOpacity>
 
-      <ScrollView>
-        <SafeAreaView>
-          <View>
+        <View>
             <Image
               style={styles.image}
               source={require("../assets/ProfilePic.png")}
             ></Image>
-          </View>
+        </View>
+
+          <Text style={styles.buttonText}>My favourite best friends..</Text>
+          <Text style={styles.buttonText}>Looking for... Border Terrier</Text>
+          <Text style={styles.buttonText}>Home town... Dundee</Text>
+          
+          <Text style={styles.profileText}>Edit Profile</Text>
+
+          
+            
+      
         </SafeAreaView>
       </ScrollView>
-      <View>
-        <Text style={styles.button1}>Jayne MacDonald</Text>
-      </View>
 
-      <TouchableOpacity style={(styles.buttonRow, styles.tanrow)}>
-        <View>
-          <Text style={styles.button2}>
-            Hi,I'm Jayne, 34, live in a lovely cottage in Balmain. l have grown
-            up with 3 adorable dogs. Living with dogs for 20 years made me a big
-            dog lover and an expert of their body language. l will be at home
-            and your dog will receive lots of hugs, pets and understanding and
-            if it is feeling up for it- a walking buddy. When you are not
-            around, l will try to make it peaceful and have fun. Whilst in my
-            care I will be sure to keep you updated with lots of photos and
-            videos.{" "}
-          </Text>
-        </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={(styles.buttonRow, styles.tanrow)}>
-        <View>
-          <Text style={styles.buttonText}>Local Services</Text>
-        </View>
-      </TouchableOpacity>
+  
 
       <Footer></Footer>
     </>
@@ -80,7 +55,7 @@ const Profile = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#D8955A",
+    backgroundColor: "#66a5c7",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -92,12 +67,9 @@ const styles = StyleSheet.create({
     opacity: 0.8,
   },
   image: {
-    width: 400,
-    height: 200,
-    alignItems: "center",
-    flexGrow: 1,
-    flex: 1,
-    justifyContent: "center",
+    padding: 10,
+    flex:1,
+    justifyContent: 'center',
   },
   buttonRow: {
     flex: 1,
@@ -108,23 +80,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 20,
     fontWeight: "300",
-    backgroundColor: "#4DA49C",
+    backgroundColor: "#D07A38",
     color: "white",
     paddingVertical: 15,
-  },
-  button2: {
-    color: "black",
-    width: "100%",
-    height: 100,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-  button1: {
-    color: "white",
-    width: "100%",
-    height: 100,
-    backgroundColor: "#e3924e",
-    justifyContent: "center",
   },
   // titleContainer {
   //   flex: 1,
@@ -140,6 +98,12 @@ const styles = StyleSheet.create({
   //   flexWrap: "wrap",
   //   justifyContent: "center",
   // },
+  // button2: {
+  //   width: "100%",
+  //   height: 100,
+  //   backgroundColor: "#a4c1db",
+  //   justifyContent: "center",
+  // },
   // leftBox: {
   //   backgroundColor: "#e3924e",
   //   alignItems: "center",
@@ -151,6 +115,12 @@ const styles = StyleSheet.create({
   //   alignItems: "center",
   //   margin: 0,
   //   width: "50%",
+  // },
+  // button1: {
+  //   width: "100%",
+  //   height: 100,
+  //   backgroundColor: "#e3924e",
+  //   justifyContent: "center",
   // },
   // mintButton: {
   //   backgroundColor: "#19A79C",
